75caf965c1cab52da2cc2dffe36a20cecfd9a8bd
Atika2002 <atikaa_20@hotmail.com>
2025-06-04 21:28:00 +0200

    Hei

--- src/Program.java
+++ src/Program.java
@@ -1,2 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
+    private final DBService dbService;
+    public Program() {
+        dbService = new DBService();
+    }
+
+    public void lesFunn() throws FileNotFoundException, SQLException {
+        File funnFile = new File(".txt");
+        Scanner reader = new Scanner(funnFile);
+        try (Connection connection = dbService.getDataSource().getConnection()) {
+            while (reader.hasNextLine()) {
+                String overskrift = reader.nextLine();
+                switch (overskrift) {
+                    //case "Personer:" ->
+                    //case "Museer:" -> {
+                    }
+                    reader.nextLine();
+                }
+            }
+        reader.close();
+    }
+
+
+
 }
